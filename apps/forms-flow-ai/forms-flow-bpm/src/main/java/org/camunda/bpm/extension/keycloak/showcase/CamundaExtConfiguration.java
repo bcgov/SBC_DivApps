63901f5a80063232c5af4fd00bf98b3a1fb4b585
sumathi-thirumani-aot <sumathi.thirumani@aot-technologies.com>
2021-05-20 22:32:19 -0700

    Changes to push the analytics configuration in divapps.

---
+++ apps/forms-flow-ai/forms-flow-bpm/src/main/java/org/camunda/bpm/extension/keycloak/showcase/CamundaExtConfiguration.java
@@ -0,0 +1,44 @@
+package org.camunda.bpm.extension.keycloak.showcase;
+
+
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+
+import javax.sql.DataSource;
+
+
+/**
+ * The Camunda Showcase Spring Boot application.
+ */
+@Configuration
+public class CamundaExtConfiguration {
+
+	/**
+	 * Secondary datasource.
+	 * This is used only for publishing data to analytics.
+	 * @return
+	 */
+	@Bean("analyticsDS")
+	@ConfigurationProperties("analytics.datasource")
+	public DataSource analyticsDS(){
+		return DataSourceBuilder.create().build();
+	}
+
+
+	/**
+	 * JDBC template for analytics datasource interaction.
+	 * @param analyticsDS
+	 * @return
+	 */
+	@Bean("analyticsJdbcTemplate")
+	public NamedParameterJdbcTemplate analyticsJdbcTemplate(@Qualifier("analyticsDS") DataSource analyticsDS) {
+		return new NamedParameterJdbcTemplate(analyticsDS);
+	}
+
+
+}
