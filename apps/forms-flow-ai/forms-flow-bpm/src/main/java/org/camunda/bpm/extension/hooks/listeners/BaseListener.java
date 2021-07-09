fa263fa7fbe9d4ad159d7526a3b97a5c414c35a3
sumathi-thirumani-aot <57740789+sumathi-thirumani-aot@users.noreply.github.com>
2021-07-08 18:54:05 -0700

    Changes to receive notification on application failures. (#113)
    * Code segregation.
    
    * Redis Message Broker Implementation
    
    * Redis Message Broker Implementation
    
    * Redis Message Broker Implementation
    
    * Redis Message Broker Implementation
    
    * Redis Message Broker Implementation
    
    * Redis Message Broker Implementation
    
    * Changes to support notification on api errors.
    
    * Changes to support notification on api errors.

---
+++ apps/forms-flow-ai/forms-flow-bpm/src/main/java/org/camunda/bpm/extension/hooks/listeners/BaseListener.java
@@ -0,0 +1,60 @@
+package org.camunda.bpm.extension.hooks.listeners;
+
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.variable.Variables;
+import org.camunda.bpm.extension.hooks.services.IMessageEvent;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Base class to apply default behavior to listeners.
+ *
+ * @author sumathi.thirumani@aot-technologies.com
+ */
+public class BaseListener implements IMessageEvent {
+
+    private final Logger LOGGER = Logger.getLogger(BaseListener.class.getName());
+
+    protected void handleException(DelegateExecution execution, ExceptionSource category, Exception e) {
+        notifyForAttention(execution, ExceptionUtils.getRootCauseMessage(e));
+        if(ExceptionSource.EXECUTION.name().equals(category.name())) {
+            handleExecutionException(e);
+        }
+        if(ExceptionSource.TASK.name().equals(category.name())) {
+            handleTaskException(e);
+        }
+    }
+
+    private void handleExecutionException(Exception e) {
+        throw new RuntimeException(ExceptionUtils.getRootCause(e));
+    }
+
+    private void handleTaskException(Exception e) { throw new RuntimeException(ExceptionUtils.getRootCause(e));
+    }
+
+
+    private void notifyForAttention(DelegateExecution execution, String exceptionMessage){
+        Map<String,Object> exVarMap = new HashMap<>();
+        //Additional Response Fields - BEGIN
+        exVarMap.put("pid",execution.getProcessInstanceId());
+        exVarMap.put("subject",("Exception Occurred ").concat(" for id: ").concat(execution.getProcessInstanceId()));
+        exVarMap.put("category","api_service_exception");
+        exVarMap.put("exception", Variables.stringValue(exceptionMessage,true));
+        //Additional Response Fields - END
+        sendMessage(execution,exVarMap,getMessageName());
+        LOGGER.info("\n\nMessage sent! " + "\n\n");
+    }
+
+    private String getMessageName(){
+        return "Service_Api_Message_Email";
+    }
+
+    public enum ExceptionSource {
+        TASK,
+        EXECUTION;
+    }
+}
