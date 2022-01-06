0a38bb5e98752fed9e715326aa51895bcaff3ce1
Krishnan Subramanian <krishnan.s@aot-technologies.com>
2022-01-05 19:38:13 -0800

    Copying changes from 4.0.3

--- apps/forms-flow-ai/forms-flow-bpm/src/main/java/org/camunda/bpm/extension/commons/connector/support/ApplicationAccessHandler.java
+++ apps/forms-flow-ai/forms-flow-bpm/src/main/java/org/camunda/bpm/extension/commons/connector/support/ApplicationAccessHandler.java
@@ -1,11 +1,12 @@
 package org.camunda.bpm.extension.commons.connector.support;
 
 import com.google.gson.JsonObject;
-// import org.slf4j.Logger;
-import java.util.logging.Logger;
-// import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.*;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
@@ -21,21 +22,22 @@ import static org.springframework.security.oauth2.client.web.reactive.function.c
 @Service("applicationAccessHandler")
 public class ApplicationAccessHandler implements IAccessHandler {
 
-    // private final Logger LOGGER = LoggerFactory.getLogger(ApplicationAccessHandler.class);
-    private final Logger LOGGER = Logger.getLogger(ApplicationAccessHandler.class.getName());
+    private final Logger LOGGER = LoggerFactory.getLogger(ApplicationAccessHandler.class);
+    
+    @Autowired
+    private WebClient unAuthenticatedWebClient;
 
     @Autowired
-    private WebClient webClient;
+    private OAuth2RestTemplate oAuth2RestTemplate;
+
 
     public ResponseEntity<String> exchange(String url, HttpMethod method, String payload) {
 
         payload = (payload == null) ? new JsonObject().toString() : payload;
 
-        LOGGER.info("Application url::" + url + " method::" + method + " payload::" + payload );
-        LOGGER.info("clientRegistrationId::" + clientRegistrationId("keycloak-client"));
-        Mono<ResponseEntity<String>> entityMono = webClient.method(method).uri(url)
-                .attributes(clientRegistrationId("keycloak-client"))
+        Mono<ResponseEntity<String>> entityMono = unAuthenticatedWebClient.method(method).uri(url)
                 .accept(MediaType.APPLICATION_JSON)
+                .headers(httpHeaders -> httpHeaders.setBearerAuth(oAuth2RestTemplate.getAccessToken().getValue()))
                 .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .body(Mono.just(payload), String.class)
                 .retrieve()
