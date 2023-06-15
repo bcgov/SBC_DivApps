1a7762a08c2ff7e9c2ab9e80a074670396157248
dinesh <dinesh.pb@aot-technologies.com>
2023-06-15 14:14:32 -0700

    group roles changed for application and submission access

---
+++ apps/forms-flow-ai/forms-flow-web/src/constants/groupConstants.js
@@ -0,0 +1,4 @@
+export const GROUPS={
+  applicationsAccess:["/formsflow/access-allow-applications","/formsflow/formsflow-client/access-allow-applications"],
+  viewSubmissionsAccess:["/formsflow/access-allow-submissions"]
+};
