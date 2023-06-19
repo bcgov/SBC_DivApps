9f42f08ac677f7f1d5ec97a2b69257f45ae0c516
Ijeoma Sophia <ijeoma.oforeh@aot-technologies.com>
2023-06-19 11:33:13 -0600

    updates made to keycloak plugin files

--- apps/forms-flow-ai/forms-flow-bpm/src/main/java/org/camunda/bpm/extension/keycloak/plugin/KeycloakIdentityProviderSession.java
+++ apps/forms-flow-ai/forms-flow-bpm/src/main/java/org/camunda/bpm/extension/keycloak/plugin/KeycloakIdentityProviderSession.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import org.camunda.bpm.engine.identity.Group;
 import org.camunda.bpm.engine.identity.User;
+import org.camunda.bpm.engine.identity.Tenant;
+import org.camunda.bpm.engine.identity.TenantQuery;
 import org.camunda.bpm.engine.impl.interceptor.CommandContext;
 import org.camunda.bpm.extension.keycloak.*;
 import org.camunda.bpm.extension.keycloak.cache.QueryCache;
@@ -19,13 +21,23 @@ import org.springframework.util.StringUtils;
 public class KeycloakIdentityProviderSession
 		extends org.camunda.bpm.extension.keycloak.KeycloakIdentityProviderSession {
 
-	public KeycloakIdentityProviderSession(KeycloakConfiguration keycloakConfiguration, KeycloakRestTemplate restTemplate, KeycloakContextProvider keycloakContextProvider,
-			QueryCache<CacheableKeycloakUserQuery, List<User>> userQueryCache, QueryCache<CacheableKeycloakGroupQuery, List<Group>> groupQueryCache,
-			QueryCache<CacheableKeycloakCheckPasswordCall, Boolean> checkPasswordCache,
-			String webClientId, boolean enableClientAuth) {
+	private CustomConfig config;
+	private TenantService tenantService;
+	protected QueryCache<CacheableKeycloakTenantQuery, List<Tenant>> tenantQueryCache;
+
+	public KeycloakIdentityProviderSession(KeycloakConfiguration keycloakConfiguration, 
+			KeycloakRestTemplate restTemplate, KeycloakContextProvider keycloakContextProvider,
+			QueryCache<CacheableKeycloakUserQuery, List<User>> userQueryCache, 
+			QueryCache<CacheableKeycloakGroupQuery, List<Group>> groupQueryCache,
+			QueryCache<CacheableKeycloakTenantQuery, List<Tenant>> tenantQueryCache,
+			QueryCache<CacheableKeycloakCheckPasswordCall, Boolean> checkPasswordCache, CustomConfig config) {
+			
 		super(keycloakConfiguration, restTemplate, keycloakContextProvider, userQueryCache, groupQueryCache, checkPasswordCache);
-		this.groupService = new  KeycloakGroupService(keycloakConfiguration, restTemplate, keycloakContextProvider, webClientId, enableClientAuth);
-        this.userService = new KeycloakUserService(keycloakConfiguration, restTemplate, keycloakContextProvider, webClientId, enableClientAuth);
+		this.config = config;
+		this.groupService = new KeycloakGroupService(keycloakConfiguration, restTemplate, keycloakContextProvider, config);
+        this.userService = new KeycloakUserService(keycloakConfiguration, restTemplate, keycloakContextProvider, config);
+		this.tenantService = new TenantService(restTemplate, keycloakContextProvider, config);
+		this.tenantQueryCache = tenantQueryCache;
 	}
 
 	/**
@@ -74,5 +86,85 @@ public class KeycloakIdentityProviderSession
 				this.userService.requestUsersByGroupId(userQuery) :
 				this.userService.requestUsersWithoutGroupId(userQuery);
 	}
-	
+
+		/**
+	 * find groups meeting given group query criteria (with cache lookup and post
+	 * processing).
+	 * 
+	 * @param groupQuery the group query
+	 * @return list of matching groups
+	 */
+	protected List<Group> findGroupByQueryCriteria(KeycloakGroupQuery groupQuery) {
+		StringBuilder resultLogger = new StringBuilder();
+
+		if (KeycloakPluginLogger.INSTANCE.isDebugEnabled()) {
+			resultLogger.append("Keycloak group query results: [");
+		}
+
+		List<Group> allMatchingGroups = groupQueryCache.getOrCompute(CacheableKeycloakGroupQuery.of(groupQuery),
+				this::doFindGroupByQueryCriteria);
+
+		List<Group> processedGroups = groupService.postProcessResults(groupQuery, allMatchingGroups, resultLogger);
+
+		if (KeycloakPluginLogger.INSTANCE.isDebugEnabled()) {
+			resultLogger.append("]");
+			KeycloakPluginLogger.INSTANCE.groupQueryResult(resultLogger.toString());
+		}
+
+		return processedGroups;
+	}
+
+	/**
+	 * find all groups meeting given group query criteria (without cache lookup or
+	 * post processing).
+	 * 
+	 * @param groupQuery the group query
+	 * @return list of matching groups
+	 */
+	private List<Group> doFindGroupByQueryCriteria(CacheableKeycloakGroupQuery groupQuery) {
+		if (StringUtils.hasLength(groupQuery.getUserId())) {
+			// if restriction on userId is provided, we're searching within the groups of a
+			// single user
+			return groupService.requestGroupsByUserId(groupQuery);
+		} else {
+			return groupService.requestGroupsWithoutUserId(groupQuery);
+		}
+	}
+
+	@Override
+	public TenantQuery createTenantQuery() {
+		return new CustomKeycloakTenantQuery(org.camunda.bpm.engine.impl.context.Context.getProcessEngineConfiguration()
+				.getCommandExecutorTxRequired());
+	}
+
+	@Override
+	public TenantQuery createTenantQuery(CommandContext commandContext) {
+		return new CustomKeycloakTenantQuery();
+	}
+
+	protected long findTenantCountByQueryCriteria(CustomKeycloakTenantQuery tenantQuery) {
+		return findTenantByQueryCriteria(tenantQuery).size();
+	}
+
+	private List<Tenant> doFindTenantByQueryCriteria(CacheableKeycloakTenantQuery tenantQuery) {
+		if (!config.isEnableMultiTenancy()) {
+			return Collections.emptyList();
+		}
+		return tenantService.requestTenantsByUserId(tenantQuery.getUserId());
+	}
+
+	protected List<Tenant> findTenantByQueryCriteria(CustomKeycloakTenantQuery tenantQuery) {
+
+		List<Tenant> allMatchingTenants = tenantQueryCache.getOrCompute(CacheableKeycloakTenantQuery.of(tenantQuery),
+				this::doFindTenantByQueryCriteria);
+
+		return allMatchingTenants;
+	}
+
+	@Override
+	public Tenant findTenantById(String id) {
+		return createTenantQuery(org.camunda.bpm.engine.impl.context.Context.getCommandContext()).tenantId(id)
+				.singleResult();
+	}
+
 }
