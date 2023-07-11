fa5ee1880cbe840c89a8b85ac2f1dccacc576fb4
Ijeoma Sophia <ijeoma.oforeh@aot-technologies.com>
2023-07-11 14:21:11 -0600

    all changes to NavBar.jsx reverted

--- apps/forms-flow-ai/forms-flow-web/src/containers/NavBar.jsx
+++ apps/forms-flow-ai/forms-flow-web/src/containers/NavBar.jsx
@@ -1,97 +1,34 @@
-import React, { useEffect, useMemo, useState } from "react";
+import React from "react";
 import {Navbar, Dropdown, Container, Nav, NavDropdown} from "react-bootstrap";
 import {Link, useLocation} from "react-router-dom";
 import {useDispatch, useSelector} from "react-redux";
 import UserService from "../services/UserService";
-import {
-    getUserRoleName, 
-    getUserRolePermission, 
-    getUserInsightsPermission
-} from "../helper/user";
-import createURLPathMatchExp from "../helper/regExp/pathMatch";
-import { useTranslation } from "react-i18next";
+import {getUserRoleName, getUserRolePermission, getUserInsightsPermission} from "../helper/user";
 
 import "./styles.scss";
-import {
-    CLIENT,
-    STAFF_REVIEWER,
-    APPLICATION_NAME,
-    STAFF_DESIGNER,
-    MULTITENANCY_ENABLED,
-  } from "../constants/constants";
-import { push } from "connected-react-router";
-import i18n from "../resourceBundles/i18n";
-import { setLanguage } from "../actions/languageSetAction";
-import { updateUserlang } from "../apiManager/services/userservices";  
-import { fetchSelectLanguages } from "../apiManager/services/languageServices";
-  
+import {CLIENT, STAFF_REVIEWER, APPLICATION_NAME, STAFF_DESIGNER} from "../constants/constants";
 import ServiceFlowFilterListDropDown from "../components/ServiceFlow/filter/ServiceTaskFilterListDropDown";
+import {push} from "connected-react-router";
 
 const NavBar = React.memo(() => {
   const isAuthenticated = useSelector((state) => state.user.isAuthenticated);
   const location = useLocation();
   const { pathname } = location;
   const user = useSelector((state) => state.user.userDetail);
-  const lang = useSelector((state) => state.user.lang);
   const userRoles = useSelector((state) => state.user.roles);
   const showApplications= useSelector((state) => state.user.showApplications);
-  const applicationTitle = useSelector(
-    (state) => state.tenants?.tenantData?.details?.applicationTitle
-  );
-  const tenantKey = useSelector((state) => state.tenants?.tenantId);
-  const formTenant = useSelector((state)=>state.form?.form?.tenantKey);
-  const baseUrl = MULTITENANCY_ENABLED ? `/tenant/${tenantKey}/` : "/";
-
-  /**
-   * For anonymous forms the only way to identify the tenant is through the
-   * form data with current implementation. To redirect to the correact tenant
-   * we will use form as the data source for the tenantKey
-   */
-
-  const [loginUrl, setLoginUrl] = useState(baseUrl);
-
-  const selectLanguages = useSelector((state) => state.user.selectLanguages);
   const dispatch = useDispatch();
   const logoPath = "/logo.svg";
-  const getAppName = useMemo(
-    () => () => {
-      if (!MULTITENANCY_ENABLED) {
-        return APPLICATION_NAME;
-      }
-      // TODO: Need a propper fallback component prefered a skeleton.
-      return applicationTitle || "";
-    },
-    [MULTITENANCY_ENABLED, applicationTitle]
-  );
-  const appName = getAppName();
-  const { t } = useTranslation();
+  const appName = APPLICATION_NAME;
 
-  useEffect(()=>{
-    if(!isAuthenticated && formTenant && MULTITENANCY_ENABLED){ 
-      setLoginUrl(`/tenant/${formTenant}/`);
-    }
-  },[isAuthenticated, formTenant]);
-
-  useEffect(() => {
-    dispatch(fetchSelectLanguages());
-  }, [dispatch]);
-
-  useEffect(() => {
-    i18n.changeLanguage(lang);
-  }, [lang]);
-
-  const handleOnclick = (selectedLang) => {
-    dispatch(setLanguage(selectedLang));
-    dispatch(updateUserlang(selectedLang));
-  };
   const logout = () => {
-      dispatch(push(baseUrl));
+      dispatch(push(`/`));
       UserService.userLogout();
-  };
+  }
 
   const goToTask = () => {
-    dispatch(push(`${baseUrl}task`));
-  };
+    dispatch(push(`/task`));
+  }
 
   return (
     <header>
@@ -103,7 +40,7 @@ const NavBar = React.memo(() => {
             </div>
           </Nav>*/}
           <Navbar.Brand className="d-flex" >
-            <Link to={`${baseUrl}`}>
+            <Link to="/">
               <img
                 className="img-fluid"
                 src={logoPath}
@@ -127,15 +64,15 @@ const NavBar = React.memo(() => {
             </Link>
           </Navbar.Brand>*/}
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-         { isAuthenticated ? (
+          {isAuthenticated?
             <Navbar.Collapse id="responsive-navbar-nav" className="navbar-nav">
             <Nav id="main-menu-nav" className="mr-auto">
-              <Nav.Link eventKey="1" as={Link} to={`${baseUrl}form`}  className={`main-nav nav-item ${
-                pathname.match(createURLPathMatchExp("form", baseUrl)) ? "" : "inactive-tab"
+              <Nav.Link eventKey="1" as={Link} to='/form'  className={`main-nav nav-item ${
+                pathname.match(/^\/form/) ? "" : "inactive-tab"
               }`}>  
                 <i
                   className={`fa fa-wpforms fa-lg
-                      ${pathname.match(createURLPathMatchExp("form", baseUrl)) ? "active-tab-text" : ""}
+                      ${pathname.match(/^\/form/) ? "active-tab-text" : ""}
                   `}
                 >
                 </i>
@@ -144,110 +81,74 @@ const NavBar = React.memo(() => {
                               ${pathname.match(/^\/form/) ? "active-tab-text" : ""}`
                             }
                 >
-                  {t("Forms")}
+                  Forms
                 </span>
               </Nav.Link>
               {(getUserRolePermission(userRoles, STAFF_DESIGNER)) ?
-                (<Nav.Link eventKey="10" 
-                as={Link} 
-                to={`${baseUrl}admin`} 
-                className={`main-nav nav-item ${
-                  pathname.match(createURLPathMatchExp("form", baseUrl)) 
-                  ? "" 
-                  : "inactive-tab"
+                (<Nav.Link eventKey="10" as={Link} to='/admin'  className={`main-nav nav-item ${
+                  pathname.match(/^\/admin/) ? "" : "inactive-tab"
                 }`}>
                   <i
                     className={`fa fa-list-alt fa-fw fa-lg
-                        ${pathname.match(createURLPathMatchExp("admin", baseUrl)) ? "active-tab-text" : ""}`
+                        ${pathname.match(/^\/admin/) ? "active-tab-text" : ""}`
                     }
                   >
                   </i> 
                   <span 
                     className={`tab-text-padding 
-                                ${pathname.match(createURLPathMatchExp("admin", baseUrl)) ? "active-tab-text" : ""}`
+                                ${pathname.match(/^\/admin/) ? "active-tab-text" : ""}`
                               }
                   >
-                    {t("Admin")}
+                    Admin
                   </span>
                 </Nav.Link>)
                 :null
               }
 
-                {getUserRolePermission(userRoles, STAFF_DESIGNER) ? (
-                  <Nav.Link
-                    as={Link}
-                    to={`${baseUrl}processes`}
-                    className={`main-nav nav-item ${
-                      pathname.match(
-                        createURLPathMatchExp("processes", baseUrl)
-                      )
-                      ? "" 
-                      : "inactive-tab"
-                    }`}
-                  >
-                    <i
-                    className={`fa fa-cogs fa-fw fa-lg
-                        ${pathname.match(createURLPathMatchExp("processes", baseUrl)) ? "active-tab-text" : ""}`
-                    }
-                  >
-                  </i> 
-                    {t("Processes")}
-                  </Nav.Link>
-                ) : null}
-
-              {showApplications ? (
-                getUserRolePermission(userRoles, STAFF_REVIEWER) ||  
-                getUserRolePermission(userRoles, CLIENT)) ?
-                <Nav.Link 
-                eventKey="2" 
-                as={Link} 
-                to={`${baseUrl}application`}  
-                className={`main-nav nav-item ${
-                  pathname.match(
-                    createURLPathMatchExp("application", baseUrl)
-                    ) 
-                    ? "" 
-                    : pathname.match(
-                        createURLPathMatchExp("draft", baseUrl)
-                      )
-                      ? ""
-                      : "inactive-tab"
-
+              {showApplications?(getUserRolePermission(userRoles, STAFF_REVIEWER) ||  getUserRolePermission(userRoles, CLIENT)) ?
+                <Nav.Link eventKey="2" as={Link} to='/application'  className={`main-nav nav-item ${
+                  pathname.match(/^\/application/) ? "" : "inactive-tab"
                 }`}> 
                   <img
                     className={`applications-icon-header
-                                ${pathname.match(createURLPathMatchExp("application", baseUrl)) ? "active-tab" : ""}
+                                ${pathname.match(/^\/application/) ? "active-tab" : ""}
                                 `}
                     src="/webfonts/fa-regular_list-alt.svg"
                     alt="Header Applications Icon"
                   /> 
                   <span
                     className={`tab-text-padding 
-                    ${pathname.match(createURLPathMatchExp("application", baseUrl)) ? "active-tab-text" : ""}`
+                    ${pathname.match(/^\/application/) ? "active-tab-text" : ""}`
                   }
                   >
-                    {t("Applications")}
+                    Applications
                   </span>
                 </Nav.Link>
                 :
                 null:
                 null}
 
+{/*              {getUserRolePermission(userRoles, STAFF_REVIEWER) ?
+                <Nav.Link as={Link} to='/task'  className={`main-nav nav-item ${
+                  pathname.match(/^\/task/) ? "active-tab" : ""
+                }`}><i className="fa fa-list"/> Tasks</Nav.Link>
+                :
+                null}*/}
 
-            {/* {getUserRolePermission(userRoles, STAFF_REVIEWER) ?
+              {getUserRolePermission(userRoles, STAFF_REVIEWER) ?
                 <NavDropdown
                   title={
                       <span className="white-text">
                         <img
                           className={`task-dropdown-icon
-                                      ${pathname.match(createURLPathMatchExp("task", baseUrl)) ? "active-tab-dropdown" : "inactive-tab"}`
+                                      ${pathname.match(/^\/task/) ? "active-tab-dropdown" : "inactive-tab"}`
                                     }
                           src="/webfonts/fa-solid_list.svg"
                           alt="Task Icon"
                         /> 
                         <span
                           className={`tab-text-padding 
-                                    ${pathname.match(createURLPathMatchExp("task", baseUrl)) ? "active-tab-text" : ""}
+                                    ${pathname.match(/^\/task/) ? "active-tab-text" : ""}
                           `}
                         >
                             Tasks
@@ -256,37 +157,14 @@ const NavBar = React.memo(() => {
                   }
                   id="task-dropdown"
                   className={`main-nav nav-item taskDropdown
-                            ${pathname.match(createURLPathMatchExp("task", baseUrl)) ? "" : "inactive-tab"}`
+                            ${pathname.match(/^\/task/) ? "" : "inactive-tab"}`
                   }
                   onClick={goToTask}
                 >
                   <ServiceFlowFilterListDropDown/>
-                </NavDropdown>:null} */}
+              </NavDropdown>:null}
 
-             {getUserRolePermission(userRoles, STAFF_REVIEWER) ? (
-                  <Nav.Link
-                    as={Link}
-                    to={`${baseUrl}task`}
-                    className={`main-nav nav-item taskDropdown ${
-                      pathname.match(createURLPathMatchExp("task", baseUrl))
-                      ? ""
-                      : "inactive-tab"
-                    }`}
-                    onClick={goToTask}
-                  >
-                    {" "}
-                    <img
-                          className={`task-dropdown-icon
-                                      ${pathname.match(createURLPathMatchExp("task", baseUrl)) ? "active-tab-dropdown" : "inactive-tab"}`
-                                    }
-                          src="/webfonts/fa-solid_list.svg"
-                          alt="Task Icon"
-                        /> 
-                    {t("Tasks")}
-                  </Nav.Link>
-                ) : null}
-
-                    {/*} {getUserRolePermission(userRoles, STAFF_REVIEWER) ?
+              {getUserRolePermission(userRoles, STAFF_REVIEWER) ?
                 <NavDropdown 
                   title={
                   <span className="white-text">
@@ -348,29 +226,10 @@ const NavBar = React.memo(() => {
                       <span className={`${pathname.match(/^\/insights/) ? "dropdown-option-selected" : "black-text"}`}>
                         Insights
                       </span>
-                    </NavDropdown.Item>} */}
-
-                {getUserRolePermission(userRoles, STAFF_REVIEWER) ? (
-                  <Nav.Link
-                    as={Link}
-                    to={`${baseUrl}metrics`}
-                    data-testid="Dashboards"
-                    className={`main-nav nav-item ${
-                      pathname.match(
-                        createURLPathMatchExp("metrics", baseUrl)
-                      ) ||
-                      pathname.match(createURLPathMatchExp("insights", baseUrl))
-                      ? ""
-                      : "inactive-tab"
-                    }`}
-                  >
-                    {" "}
-                    <i className="fa fa-tachometer fa-2 dashboard-icon-dropdown" />
-                    {t("Dashboards")}
-                  </Nav.Link>
-                ) : null}
+                    </NavDropdown.Item>
+                  }
+              </NavDropdown>:null}
             </Nav>
-   
             <Nav className="ml-auto">
               <Dropdown alignRight>
                 <Dropdown.Toggle id="dropdown-basic" as="div">
@@ -394,12 +253,7 @@ const NavBar = React.memo(() => {
                 </Dropdown.Menu>
               </Dropdown>
               </Nav>
-          </Navbar.Collapse>
-          ) : (
-          <Link to="/" className="btn btn-primary">
-            Login
-          </Link>
-          )}
+          </Navbar.Collapse>:<Link to="/" className="btn btn-primary">Login</Link>}
         </Container>
       </Navbar>
     </header>
