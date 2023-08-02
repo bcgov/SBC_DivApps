ec8c45f1eb8956047aeb184a25ba2b96feaa9df6
Ijeoma Sophia <ijeoma.oforeh@aot-technologies.com>
2023-08-02 15:43:43 -0600

    files updated

--- apps/forms-flow-ai/forms-flow-bpm/src/main/java/org/camunda/bpm/extension/commons/connector/support/ApplicationAccessHandler.java
+++ apps/forms-flow-ai/forms-flow-bpm/src/main/java/org/camunda/bpm/extension/commons/connector/support/ApplicationAccessHandler.java
@@ -1,30 +1,31 @@
 package org.camunda.bpm.extension.commons.connector.support;
 
 import com.google.gson.JsonObject;
+import org.camunda.bpm.extension.commons.ro.req.IRequest;
+import org.camunda.bpm.extension.commons.ro.res.IResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+// import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.*;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
+
+import java.util.Map;
 
 import static org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction.clientRegistrationId;
 
 /**
  * This class serves as gateway for all application service interactions.
- * This customization exists because the authentication mechanisms has changed
- * between
- * forsmflow open source and service bc's implementation. Once Service BC's
- * authentication
- * mechanisms are updated, this file can be removed.
  *
  * @author sumathi.thirumani@aot-technologies.com
  */
 @Service("applicationAccessHandler")
-public class ApplicationAccessHandler implements IAccessHandler {
+public class ApplicationAccessHandler extends AbstractAccessHandler {
 
         private final Logger LOGGER = LoggerFactory.getLogger(ApplicationAccessHandler.class);
 
@@ -38,17 +39,35 @@ public class ApplicationAccessHandler implements IAccessHandler {
 
                 payload = (payload == null) ? new JsonObject().toString() : payload;
 
-                Mono<ResponseEntity<String>> entityMono = unAuthenticatedWebClient.method(method).uri(url)
+                ResponseEntity<String> response = unAuthenticatedWebClient.method(method).uri(url)
                                 .accept(MediaType.APPLICATION_JSON)
                                 .headers(httpHeaders -> httpHeaders
                                                 .setBearerAuth(oAuth2RestTemplate.getAccessToken().getValue()))
                                 .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                                 .body(Mono.just(payload), String.class)
                                 .retrieve()
-                                .toEntity(String.class);
+                                .toEntity(String.class)
+                                .block();
 
-                ResponseEntity<String> response = entityMono.block();
+                // ResponseEntity<String> response = entityMono.block();
                 return new ResponseEntity<>(response.getBody(), response.getStatusCode());
         }
 
+        public ResponseEntity<IResponse> exchange(String url, HttpMethod method, IRequest payload,
+                        Class<? extends IResponse> responseClazz) {
+
+                ResponseEntity<? extends IResponse> response = unAuthenticatedWebClient.method(method).uri(url)
+                                .accept(MediaType.APPLICATION_JSON)
+                                .headers(httpHeaders -> httpHeaders
+                                                .setBearerAuth(oAuth2RestTemplate.getAccessToken().getValue()))
+                                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                                .body((payload == null ? BodyInserters.empty() : BodyInserters.fromValue(payload)))
+                                .retrieve()
+                                .onStatus(HttpStatus::is4xxClientError,
+                                                clientResponse -> Mono.error(
+                                                                new HttpClientErrorException(HttpStatus.BAD_REQUEST)))
+                                .toEntity(responseClazz)
+                                .block();
+                return new ResponseEntity<>(response.getBody(), response.getStatusCode());
+        }
 }
