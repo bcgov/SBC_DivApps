729ab086b011a7dc1dcd13851ab78592ac6ff7c4
Ijeoma Sophia <ijeoma.oforeh@aot-technologies.com>
2023-08-02 13:42:33 -0600

    ApplicationAccessHandler.java changes reverted

--- apps/forms-flow-ai/forms-flow-bpm/src/main/java/org/camunda/bpm/extension/commons/connector/support/ApplicationAccessHandler.java
+++ apps/forms-flow-ai/forms-flow-bpm/src/main/java/org/camunda/bpm/extension/commons/connector/support/ApplicationAccessHandler.java
@@ -6,9 +6,7 @@ import org.camunda.bpm.extension.commons.ro.res.IResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.*;
-import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.reactive.function.BodyInserters;
@@ -20,46 +18,59 @@ import java.util.Map;
 import static org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction.clientRegistrationId;
 
 /**
+ * Application Access Handler.
  * This class serves as gateway for all application service interactions.
- *
- * @author sumathi.thirumani@aot-technologies.com
  */
 @Service("applicationAccessHandler")
-public class ApplicationAccessHandler implements IAccessHandler {
+public class ApplicationAccessHandler extends AbstractAccessHandler {
 
         private final Logger LOGGER = LoggerFactory.getLogger(ApplicationAccessHandler.class);
 
         @Autowired
-        private WebClient unAuthenticatedWebClient;
+        private WebClient webClient;
 
-        @Autowired
-        private OAuth2RestTemplate oAuth2RestTemplate;
-
+        /**
+         * exchange function using json - string payload / string response
+         * 
+         * @param url
+         * @param method
+         * @param payload
+         * @return
+         */
         public ResponseEntity<String> exchange(String url, HttpMethod method, String payload) {
 
                 payload = (payload == null) ? new JsonObject().toString() : payload;
 
-                ResponseEntity<String> response = unAuthenticatedWebClient.method(method).uri(url)
+                ResponseEntity<String> response = webClient.method(method).uri(url)
+                                .attributes(clientRegistrationId("keycloak-client"))
                                 .accept(MediaType.APPLICATION_JSON)
-                                .headers(httpHeaders -> httpHeaders
-                                                .setBearerAuth(oAuth2RestTemplate.getAccessToken().getValue()))
                                 .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                                 .body(Mono.just(payload), String.class)
                                 .retrieve()
+                                .onStatus(HttpStatus::is4xxClientError,
+                                                clientResponse -> Mono.error(
+                                                                new HttpClientErrorException(HttpStatus.BAD_REQUEST)))
                                 .toEntity(String.class)
                                 .block();
 
-                // ResponseEntity<String> response = entityMono.block();
                 return new ResponseEntity<>(response.getBody(), response.getStatusCode());
         }
 
+        /**
+         * exchange function using the custom class
+         * 
+         * @param url
+         * @param method
+         * @param payload
+         * @param responseClazz
+         * @return
+         */
         public ResponseEntity<IResponse> exchange(String url, HttpMethod method, IRequest payload,
                         Class<? extends IResponse> responseClazz) {
 
-                ResponseEntity<? extends IResponse> response = unAuthenticatedWebClient.method(method).uri(url)
+                ResponseEntity<? extends IResponse> response = webClient.method(method).uri(url)
+                                .attributes(clientRegistrationId("keycloak-client"))
                                 .accept(MediaType.APPLICATION_JSON)
-                                .headers(httpHeaders -> httpHeaders
-                                                .setBearerAuth(oAuth2RestTemplate.getAccessToken().getValue()))
                                 .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                                 .body((payload == null ? BodyInserters.empty() : BodyInserters.fromValue(payload)))
                                 .retrieve()
