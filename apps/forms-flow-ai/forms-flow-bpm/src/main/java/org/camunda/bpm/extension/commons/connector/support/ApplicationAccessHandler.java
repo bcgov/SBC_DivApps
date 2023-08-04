48e9da9cb46972b6c5bd9137f6c5623a41c439b7
Ijeoma Sophia <ijeoma.oforeh@aot-technologies.com>
2023-08-04 10:34:14 -0600

    ApplicationAccessHandler.java updated

--- apps/forms-flow-ai/forms-flow-bpm/src/main/java/org/camunda/bpm/extension/commons/connector/support/ApplicationAccessHandler.java
+++ apps/forms-flow-ai/forms-flow-bpm/src/main/java/org/camunda/bpm/extension/commons/connector/support/ApplicationAccessHandler.java
@@ -14,6 +14,7 @@ import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
+import org.springframework.context.annotation.Primary;
 
 import java.util.Map;
 
@@ -24,7 +25,7 @@ import static org.springframework.security.oauth2.client.web.reactive.function.c
  *
  * @author sumathi.thirumani@aot-technologies.com
  */
-
+@Primary
 @Service("applicationAccessHandler")
 public class ApplicationAccessHandler extends AbstractAccessHandler implements IAccessHandler {
 
