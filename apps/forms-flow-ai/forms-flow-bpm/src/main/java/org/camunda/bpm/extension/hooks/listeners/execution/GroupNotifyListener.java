bb86c6e37921b69d739b437bbe52622344e6d358
Ijeoma Sophia <ijeoma.oforeh@aot-technologies.com>
2023-08-08 11:17:45 -0600

    GroupNotifyListener.java update

--- apps/forms-flow-ai/forms-flow-bpm/src/main/java/org/camunda/bpm/extension/hooks/listeners/execution/GroupNotifyListener.java
+++ apps/forms-flow-ai/forms-flow-bpm/src/main/java/org/camunda/bpm/extension/hooks/listeners/execution/GroupNotifyListener.java
@@ -19,7 +19,8 @@ import java.util.logging.Logger;
  * Assignment Task Listener to start a message event when a task is created
  * Extended to emails to additional groups of interest.
  *
- * @author yichun.zhao@aot-technologies.com, sumathi.thirumani@aot-technologies.com
+ * @author yichun.zhao@aot-technologies.com,
+ *         sumathi.thirumani@aot-technologies.com
  */
 @Component
 public class GroupNotifyListener implements ExecutionListener, IMessageEvent {
@@ -31,6 +32,9 @@ public class GroupNotifyListener implements ExecutionListener, IMessageEvent {
 
     private Expression emailGroups;
 
+    @Resource(name = "bpmObjectMapper")
+    private ObjectMapper bpmObjectMapper;
+
     /**
      * This provides the necessary information to send message.
      *
@@ -38,37 +42,38 @@ public class GroupNotifyListener implements ExecutionListener, IMessageEvent {
      */
     @Override
     public void notify(DelegateExecution execution) throws Exception {
-        List<String> toEmails =  new ArrayList<>();
+        List<String> toEmails = new ArrayList<>();
 
         if (CollectionUtils.isNotEmpty(getEmailGroups(execution))) {
             for (String entry : getEmailGroups(execution)) {
                 toEmails.addAll(getEmailsForGroup(execution, entry));
             }
         }
-        sendEmailNotification(execution, toEmails ,String.valueOf(execution.getVariables().get("taskId")));
-
+        sendEmailNotification(execution, toEmails, String.valueOf(execution.getVariables().get("taskId")));
 
     }
+
     /**
      *
      * @param execution
      * @param toEmails
      * @param taskId
      */
-    private void sendEmailNotification(DelegateExecution execution,List<String> toEmails,String taskId) {
-        String toAddress = CollectionUtils.isNotEmpty(toEmails) ? StringUtils.join(toEmails,",") : null;
-        if(StringUtils.isNotEmpty(toAddress)) {
+    private void sendEmailNotification(DelegateExecution execution, List<String> toEmails, String taskId) {
+        String toAddress = CollectionUtils.isNotEmpty(toEmails) ? StringUtils.join(toEmails, ",") : null;
+        if (StringUtils.isNotEmpty(toAddress)) {
             Map<String, Object> emailAttributes = new HashMap<>();
             emailAttributes.put("email_to", toAddress);
             emailAttributes.put("category", getCategory(execution));
-            emailAttributes.put("name",getDefaultAddresseName());
-            emailAttributes.put("taskid",taskId);
-            if(StringUtils.isNotBlank(toAddress) && StringUtils.indexOf(toAddress,"@") > 0) {
-                sendMessage(execution, emailAttributes,getMessageId(execution));
+            emailAttributes.put("name", getDefaultAddresseName());
+            emailAttributes.put("taskid", taskId);
+            if (StringUtils.isNotBlank(toAddress) && StringUtils.indexOf(toAddress, "@") > 0) {
+                sendMessage(execution, emailAttributes, getMessageId(execution));
             }
         }
     }
-    private String getCategory(DelegateExecution delegateExecution){
+
+    private String getCategory(DelegateExecution delegateExecution) {
         return String.valueOf(this.category.getValue(delegateExecution));
     }
 
@@ -77,31 +82,32 @@ public class GroupNotifyListener implements ExecutionListener, IMessageEvent {
      * @param delegateExecution
      * @return
      */
-    private String getMessageId(DelegateExecution delegateExecution){
+    private String getMessageId(DelegateExecution delegateExecution) {
         return String.valueOf(this.messageId.getValue(delegateExecution));
     }
 
-    private List<String> getEmailGroups(DelegateExecution delegateExecution){
+    private List<String> getEmailGroups(DelegateExecution delegateExecution) {
         List<String> emailGroups = new ArrayList<>();
         try {
-            if(this.emailGroups != null &&
+            if (this.emailGroups != null &&
                     StringUtils.isNotBlank(String.valueOf(this.emailGroups.getValue(delegateExecution)))) {
-                emailGroups = this.emailGroups != null && this.emailGroups.getValue(delegateExecution) != null ?
-                        getObjectMapper().readValue(String.valueOf(this.emailGroups.getValue(delegateExecution)), List.class) : null;
+                emailGroups = this.emailGroups != null && this.emailGroups.getValue(delegateExecution) != null
+                        ? bpmObjectMapper.readValue(String.valueOf(this.emailGroups.getValue(delegateExecution)),
+                                List.class)
+                        : null;
             }
         } catch (JsonProcessingException e) {
-            LOGGER.log(Level.SEVERE, "Exception occured in reading additionalEmailGroups" , e);
+            LOGGER.log(Level.SEVERE, "Exception occured in reading additionalEmailGroups", e);
         }
-        return  emailGroups;
+        return emailGroups;
     }
 
     /**
      * Returns Object Mapper Instance
+     * 
      * @return
      */
-    private ObjectMapper getObjectMapper(){
-        return new ObjectMapper();
-    }
-
+    // private ObjectMapper getObjectMapper() {
+    // return new ObjectMapper(); }
 
 }
