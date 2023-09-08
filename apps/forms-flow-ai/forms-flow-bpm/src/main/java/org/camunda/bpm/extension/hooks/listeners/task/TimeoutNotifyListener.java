2e68b7a672c9b8b7b8f77928f74ef25d88287588
Ijeoma Sophia <ijeoma.oforeh@aot-technologies.com>
2023-09-07 18:29:19 -0600

    TimeoutNotifyListener.java updated

--- apps/forms-flow-ai/forms-flow-bpm/src/main/java/org/camunda/bpm/extension/hooks/listeners/task/TimeoutNotifyListener.java
+++ apps/forms-flow-ai/forms-flow-bpm/src/main/java/org/camunda/bpm/extension/hooks/listeners/task/TimeoutNotifyListener.java
@@ -22,7 +22,7 @@ import org.springframework.stereotype.Component;
  * Timeout Task Listener to start a message event when the deadline is due
  */
 @Component
-public class TimeoutNotifyListener  extends BaseListener implements TaskListener, IMessageEvent {
+public class TimeoutNotifyListener extends BaseListener implements TaskListener, IMessageEvent {
 
     @Autowired
     private UserService userService;
@@ -31,7 +31,6 @@ public class TimeoutNotifyListener  extends BaseListener implements TaskListener
     private Expression currentDate;
     private static final Logger log = Logger.getLogger(TimeoutNotifyListener.class.getName());
 
-
     /**
      * This calculates time logic
      * and provides the necessary information to send message.
@@ -39,69 +38,72 @@ public class TimeoutNotifyListener  extends BaseListener implements TaskListener
      * @param delegateTask: The task which sends the message
      */
     public void notify(DelegateTask delegateTask) {
-            DateTime currentDate = this.currentDate != null && this.currentDate.getValue(delegateTask) != null ?
-                      new DateTime(String.valueOf(this.currentDate.getValue(delegateTask))) : new DateTime();
-            DateTime dueDate = delegateTask.getDueDate() != null ? new DateTime(delegateTask.getDueDate())
-                    : new DateTime(delegateTask.getExecution().getVariable("task_due_date"));
-            String escalationGroup = this.escalationGroup != null && this.escalationGroup.getValue(delegateTask) != null ?
-                    String.valueOf(this.escalationGroup.getValue(delegateTask)) : null;
-            DateTime remindDate = addBusinessDays(dueDate, -1);
-            DateTime escalateDate = addBusinessDays(dueDate,1);
-            log.info("Timeout Notify listener invoked for pid="+delegateTask.getProcessInstanceId()
-                    +"|due Date=" +dueDate.toLocalDate()+"|current Date="+currentDate.toLocalDate()
-                    +"|escalation Date="+escalateDate.toLocalDate()
-                    +"|reminder Date="+remindDate.toLocalDate());
-            // Check if escalate first because reminder date is before escalation date
-            if (currentDate.toLocalDate().equals(escalateDate.toLocalDate()) && StringUtils.isNotEmpty(escalationGroup)) {
-                validateAssigneeAndNotify(delegateTask, "activity_escalation", escalationGroup);
-            } else if (currentDate.toLocalDate().equals(remindDate.toLocalDate())) {
-                validateAssigneeAndNotify(delegateTask, "activity_reminder", null);
-            }
+        DateTime currentDate = this.currentDate != null && this.currentDate.getValue(delegateTask) != null
+                ? new DateTime(String.valueOf(this.currentDate.getValue(delegateTask)))
+                : new DateTime();
+        DateTime dueDate = delegateTask.getDueDate() != null ? new DateTime(delegateTask.getDueDate())
+                : new DateTime(delegateTask.getExecution().getVariable("task_due_date"));
+        String escalationGroup = this.escalationGroup != null && this.escalationGroup.getValue(delegateTask) != null
+                ? String.valueOf(this.escalationGroup.getValue(delegateTask))
+                : null;
+        DateTime remindDate = addBusinessDays(dueDate, -1);
+        DateTime escalateDate = addBusinessDays(dueDate, 1);
+        log.info("Timeout Notify listener invoked for pid=" + delegateTask.getProcessInstanceId()
+                + "|due Date=" + dueDate.toLocalDate() + "|current Date=" + currentDate.toLocalDate()
+                + "|escalation Date=" + escalateDate.toLocalDate()
+                + "|reminder Date=" + remindDate.toLocalDate());
+        // Check if escalate first because reminder date is before escalation date
+        if (currentDate.toLocalDate().equals(escalateDate.toLocalDate()) && StringUtils.isNotEmpty(escalationGroup)) {
+            validateAssigneeAndNotify(delegateTask, "activity_escalation", escalationGroup);
+        } else if (currentDate.toLocalDate().equals(remindDate.toLocalDate())) {
+            validateAssigneeAndNotify(delegateTask, "activity_reminder", null);
+        }
     }
 
     private void validateAssigneeAndNotify(DelegateTask delegateTask, String category, String escalationGroup) {
         List<String> escalationGrpEmails = new ArrayList<>();
-        if(StringUtils.isNotEmpty(escalationGroup)) {
-            escalationGrpEmails.addAll(getEmailsForGroup(delegateTask.getExecution(),escalationGroup));
+        if (StringUtils.isNotEmpty(escalationGroup)) {
+            escalationGrpEmails.addAll(getEmailsForGroup(delegateTask.getExecution(), escalationGroup));
         }
-        if(StringUtils.isNotEmpty(delegateTask.getAssignee())) {
+        if (StringUtils.isNotEmpty(delegateTask.getAssignee())) {
+            log.info("GetAssigneeInfo" + delegateTask.getAssignee());
             User user = getUser(delegateTask.getExecution(), userService, delegateTask.getAssignee());
-            delegateTask.getExecution().setVariable("firstname",user.getFirstName());
-            delegateTask.getExecution().setVariable("lastname",user.getLastName());
-            delegateTask.getExecution().setVariable("name",user.getFirstName()+" "+user.getLastName());
+            delegateTask.getExecution().setVariable("firstname", user.getFirstName());
+            delegateTask.getExecution().setVariable("lastname", user.getLastName());
+            delegateTask.getExecution().setVariable("name", user.getFirstName() + " " + user.getLastName());
             List<String> userEmails = new ArrayList<>();
-            if(StringUtils.isNotEmpty(user.getEmail())) {
+            if (StringUtils.isNotEmpty(user.getEmail())) {
                 userEmails.add(user.getEmail());
-                sendEmailNotification(delegateTask.getExecution(), category,userEmails
-                        , escalationGrpEmails,delegateTask.getId());
+                sendEmailNotification(delegateTask.getExecution(), category, userEmails, escalationGrpEmails,
+                        delegateTask.getId());
             }
         } else {
-            delegateTask.getExecution().setVariable("name",getDefaultAddresseName());
-            sendEmailNotification(delegateTask.getExecution(),category,
-                    getEmailsOfUnassignedTask(delegateTask), escalationGrpEmails,delegateTask.getId());
+            delegateTask.getExecution().setVariable("name", getDefaultAddresseName());
+            sendEmailNotification(delegateTask.getExecution(), category,
+                    getEmailsOfUnassignedTask(delegateTask), escalationGrpEmails, delegateTask.getId());
         }
     }
 
-    private void sendEmailNotification(DelegateExecution execution, String category, List<String> toEmails, List<String> ccEmails,String taskId) {
-        String toAddress = CollectionUtils.isNotEmpty(toEmails) ? StringUtils.join(toEmails,",") : null;
-        String ccAddress = CollectionUtils.isNotEmpty(ccEmails) ? StringUtils.join(ccEmails,",") : null;
-        if(StringUtils.isNotEmpty(toAddress)) {
+    private void sendEmailNotification(DelegateExecution execution, String category, List<String> toEmails,
+            List<String> ccEmails, String taskId) {
+        String toAddress = CollectionUtils.isNotEmpty(toEmails) ? StringUtils.join(toEmails, ",") : null;
+        String ccAddress = CollectionUtils.isNotEmpty(ccEmails) ? StringUtils.join(ccEmails, ",") : null;
+        if (StringUtils.isNotEmpty(toAddress)) {
             Map<String, Object> emailAttributes = new HashMap<>();
             emailAttributes.put("to", toAddress);
-            if(StringUtils.isNotEmpty(ccAddress)) {
+            if (StringUtils.isNotEmpty(ccAddress)) {
                 emailAttributes.put("cc", ccAddress);
             }
             emailAttributes.put("category", category);
             emailAttributes.put("taskid", taskId);
             log.info("Inside notify attributes:" + emailAttributes);
-            if(StringUtils.isNotBlank(toAddress) && StringUtils.indexOf(toAddress,"@") > 0) {
-                sendMessage(execution, emailAttributes,getMessageName(execution));
+            if (StringUtils.isNotBlank(toAddress) && StringUtils.indexOf(toAddress, "@") > 0) {
+                sendMessage(execution, emailAttributes, getMessageName(execution));
             }
         }
     }
 
-
-    private String getMessageName(DelegateExecution execution){
+    private String getMessageName(DelegateExecution execution) {
         return String.valueOf(this.messageName.getValue(execution));
     }
 
