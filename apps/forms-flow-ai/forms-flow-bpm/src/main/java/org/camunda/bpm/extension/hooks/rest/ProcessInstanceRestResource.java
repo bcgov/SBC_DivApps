ac740e8755f2b68088e9fec8b5bb78f75d596a65
Ijeoma Sophia <ijeoma.oforeh@aot-technologies.com>
2024-03-26 13:18:32 -0600

    updates in ProcessInstanceRestResource files

--- apps/forms-flow-ai/forms-flow-bpm/src/main/java/org/camunda/bpm/extension/hooks/rest/ProcessInstanceRestResource.java
+++ apps/forms-flow-ai/forms-flow-bpm/src/main/java/org/camunda/bpm/extension/hooks/rest/ProcessInstanceRestResource.java
@@ -16,29 +16,29 @@ import org.springframework.hateoas.EntityModel;
 @Produces(MediaType.APPLICATION_JSON)
 public interface ProcessInstanceRestResource extends RestResource {
 
-    String PATH = "/process-instance";
+        String PATH = "/process-instance";
 
-    public final static String DESERIALIZE_VALUE_QUERY_PARAM = "deserializeValue";
+        public final static String DESERIALIZE_VALUE_QUERY_PARAM = "deserializeValue";
 
-    @GET
-    @Path("/{id}/activity-instances")
-    @Produces(MediaType.APPLICATION_JSON)
-    EntityModel<org.camunda.bpm.engine.rest.dto.runtime.ActivityInstanceDto> getActivityInstanceTree(
-            @PathParam("id") String id);
+        @GET
+        @Path("/{id}/activity-instances")
+        @Produces(MediaType.APPLICATION_JSON)
+        EntityModel<org.camunda.bpm.engine.rest.dto.runtime.ActivityInstanceDto> getActivityInstanceTree(
+                        @PathParam("id") String id);
 
-    @GET
-    @Path("/{id}/variables")
-    @Produces(MediaType.APPLICATION_JSON)
-    Map<String, VariableValueDto> getVariables(
-            @QueryParam(DESERIALIZE_VALUE_QUERY_PARAM) @DefaultValue("true") boolean deserializeValues,
-            @PathParam("id") String id);
+        @GET
+        @Path("/{id}/variables")
+        @Produces(MediaType.APPLICATION_JSON)
+        Map<String, VariableValueDto> getVariables(
+                        @QueryParam(DESERIALIZE_VALUE_QUERY_PARAM) @DefaultValue("true") boolean deserializeValues,
+                        @PathParam("id") String id);
 
-    @GET
-    @Path("/{id}/variable/{variableName}")
-    @Produces(MediaType.APPLICATION_JSON)
-    VariableValueDto getVariable(
-            @QueryParam(DESERIALIZE_VALUE_QUERY_PARAM) @PathParam("variableName") String variableName,
-            @DefaultValue("true") boolean deserializeValue,
-            @PathParam("id") String id);
+        @GET
+        @Path("/{id}/variable/{variableName}")
+        @Produces(MediaType.APPLICATION_JSON)
+        VariableValueDto getVariable(
+                        @PathParam("variableName") String variableName,
+                        @QueryParam(DESERIALIZE_VALUE_QUERY_PARAM) @DefaultValue("true") boolean deserializeValue,
+                        @PathParam("id") String id);
 
 }
